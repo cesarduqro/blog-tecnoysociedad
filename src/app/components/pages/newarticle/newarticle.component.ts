8bd61d8893304f2f999e161544ff8a8cfa75c6e0
cduque <cesaredu4242@gmail.com>
2021-01-28 08:51:59 -0500

    Fix nuevo articulo formulario

--- src/app/components/pages/newarticle/newarticle.component.ts
+++ src/app/components/pages/newarticle/newarticle.component.ts
@@ -12,54 +12,68 @@ import Swal from 'sweetalert2';
 export class NewarticleComponent implements OnInit {
   articulo: ArticuloModel = new ArticuloModel();
   formArticulo: FormGroup;
+
   constructor(private _articuloService: ArticulosServices,
               private fb: FormBuilder) {
-    this.crearFormulario()
+    this.crearFormulario();
   }
 
   ngOnInit(): void {
 
   }
-  get idxNoValido(){
-    return this.formArticulo.get('idx').invalid && this.formArticulo.get('idx').touched
-  }
-  get tituloNoValido(){
-    return this.formArticulo.get('titulo').invalid && this.formArticulo.get('titulo').touched
-  }
-  get resumenNoValido(){
-    return this.formArticulo.get('resumen').invalid && this.formArticulo.get('resumen').touched
-  }
-  get descripcionNoValido(){
-    return this.formArticulo.get('descripcion').invalid && this.formArticulo.get('descripcion').touched
-  }
-  crearFormulario(){
-    this.formArticulo = this.fb.group({
-      idx: ['',Validators.required],
-      titulo: ['',Validators.required],
-      image: ['',Validators.required],
-      resumen: ['',Validators.required],
-      descripcion: ['',Validators.required],
 
-    })
+  get idxNoValido() {
+    return this.formArticulo.get('idx').invalid && this.formArticulo.get('idx').touched;
+  }
+
+  get tituloNoValido() {
+    return this.formArticulo.get('titulo').invalid && this.formArticulo.get('titulo').touched;
+  }
+
+  get resumenNoValido() {
+    return this.formArticulo.get('resumen').invalid && this.formArticulo.get('resumen').touched;
+  }
+
+  get descripcionNoValido() {
+    return this.formArticulo.get('descripcion').invalid && this.formArticulo.get('descripcion').touched;
+  }
+
+  crearFormulario() {
+    this.formArticulo = this.fb.group({
+        idx: ['',Validators.required],
+        titulo: ['',[Validators.required,Validators.minLength(10)]],
+        image: ['',Validators.minLength(10)],
+        resumen: ['',Validators.minLength(10)],
+        descripcion: ['',Validators.minLength(10)],
+      }
+    );
   }
 
   guardarArticulo() {
     //console.log(this.articulo)
-    if (this.formArticulo.invalid){
-      return Object.values(this.formArticulo.controls).forEach( control =>{
+    this.articulo = this.formArticulo.value;
+    if (this.formArticulo.invalid) {
+      return Object.values(this.formArticulo.controls).forEach(control => {
         control.markAsTouched();
       });
-    }else{
+    } else {
       let response: any = [];
+      console.log(this.formArticulo.status);
       this._articuloService.crearArticulo(this.articulo).subscribe(
         resp => {
           response = resp;
-          if (response.name.length > 1) {
+          if (response.name.length > 1 && this.formArticulo.status) {
             Swal.fire({
               icon: 'success',
               allowOutsideClick:false,
               text: 'Registro guardado',
               confirmButtonText: 'Ok',
+            })
+          }else{
+            Swal.fire({
+              icon: 'error',
+              allowOutsideClick:false,
+              text: 'Ha ocurrido un error',
             })
           }
           console.log(resp);
