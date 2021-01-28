48ed08ab59941347426fed85bd542e7488872643
cduque <cesaredu4242@gmail.com>
2021-01-28 09:46:35 -0500

    Acerca del blog modificado

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -8,11 +9,15 @@ export class AboutComponent implements OnInit {
   titulo: string;
   isResultsLoading: boolean = true;
   acerca_de: string;
+  descripcion: string;
 
-  constructor() {
+  constructor(private router: Router) {
     this.titulo = "Acerca del blog"
-    this.acerca_de = "Este blog fue disenado y desarrollado con el framework Angular + Typescript, todos los recursos" +
-      " usados aqui estan disponibles en el repositorio del proyecto:"
+    this.acerca_de = "Este blog fue disenado y desarrollado por https://github.com/cesarduqro.";
+    this.descripcion = "El contenido de este blog es una recopilacion de la informacion entregada en talleres y actividades en clases realizadas en" +
+      "la materia de Tecnologia y Sociedad - Universidad Ecotec.";
+
+
   }
 
   ngOnInit(): void {
