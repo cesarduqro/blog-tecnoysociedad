b7efe61d2afd8fedea2a06b62499aeb52a0ef19d
cduque <cesaredu4242@gmail.com>
2021-02-14 15:34:44 -0500

    nuevo articulo add

--- src/app/shared/navbar/sidenav.component.ts
+++ src/app/shared/navbar/sidenav.component.ts
@@ -1,33 +1,32 @@
-import {Component, OnInit} from '@angular/core';
-import {Articulos} from '../../services/articulos.services';
-import {Router} from '@angular/router';
-import {AuthServices} from '../../services/auth.services';
+import { Component, OnInit } from '@angular/core';
+import { Articulos } from '../../services/articulos.services';
+import { Router } from '@angular/router';
+import { AuthServices } from '../../services/auth.services';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
   titulo: string;
-  buscarTexto: string ;
+  buscarTexto: string;
   isLoginIn = false;
   email: string = this.servicioLogin.getLoginInfo();
   isArticleSite = false;
-  constructor(private router:Router, private servicioLogin: AuthServices) {
-    this.titulo = "Blog T&S"
+  constructor(private router: Router, private servicioLogin: AuthServices) {
+    this.titulo = 'Blog T&S';
     this.isLoginIn = servicioLogin.estaAutenticado();
-
   }
 
   ngOnInit(): void {
-    if (this.router.url === '/articles'){
+    if (this.router.url === '/articles') {
       this.isArticleSite = true;
     }
   }
 
-  buscarArticulo(termino:string){
-    this.router.navigate(['/buscar',termino])
-    termino = "";
+  buscarArticulo(termino: string) {
+    this.router.navigate(['/buscar', termino]);
+    termino = '';
   }
 }
